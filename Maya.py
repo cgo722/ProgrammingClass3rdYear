72333238d214015f4cd679dd762bf290819e615a
Camron Oakeson <holy_hades@ymail.com>
2021-10-26 10:42:12 -0600

    Snowman script finished

--- Maya.py
+++ Maya.py
@@ -12,3 +12,23 @@ cmds.polyCylinder(h=0.2, r=4)
 cmds.move(14, y=True)
 cmds.polyCylinder(h=6, r=2)
 cmds.move(17, y=True)
+#Hat model
+
+cmds.polySphere(r=0.1, n='Eye1')
+cmds.move(1, 13, 1)
+cmds.duplicate('Eye1')
+cmds.select('Eye1')
+cmds.move(1, 13, -1)
+#Eyes
+
+cmds.polyCone(h=1, r=.2)
+cmds.move(1.5, 13, 0)
+cmds.rotate(90, z=True)
+#nose
+
+cmds.polyCylinder(h=10, r=0.2, n='Arm1')
+cmds.move(0, 9, 5)
+cmds.rotate(90, x=True)
+cmds.duplicate('Arm1')
+cmds.move(0, 9, -5)
+#arms
