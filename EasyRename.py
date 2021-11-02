249d93efcdd58273966199d94e6913f5be963632
Camron Oakeson <holy_hades@ymail.com>
2021-11-02 13:10:34 -0600

    Working on figuring out the rename stuff

---
+++ EasyRename.py
@@ -0,0 +1,11 @@
+import maya.cmds as m
+
+def renameFunc(objName, objNodeType):
+    listObj = m.ls(selection=True)
+    numberPadding = "##"
+    listCount = len(listObj)
+    for count, object in enumerate(listObj):
+        m.rename(object, objName + numberPadding + objNodeType)
+        numberPadding.replace("##", str(listCount))
+
+renameFunc("sphere_","_Cheese")
